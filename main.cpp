0433b594adbd938543bb175794c19fabc3ee5cea
Skalin <DominikSkala@seznam.cz>
2017-10-23 18:21:09 +0200

    - user authentication - server wait for transaction process

--- main.cpp
+++ main.cpp
@@ -33,12 +33,56 @@ bool help = false;
 bool reset = false;
 string serverDir = "";
 string usersFile = "";
+string serverUser = "";
+string serverPass = "";
 int port = 0;
 bool isHashed = true;
-string user = "";
-string pass = "";
-int status;
 
+typedef struct {
+	string serverDir = "";
+	string usersFile = "";
+	bool isHashed = true;
+	string clientUser = "";
+	string clientPass = "";
+	int commSocket = -1;
+	string pidTimeStamp = "";
+	string tmpDir;
+	int serverStatus = 0; // unauthorized, 1 = authorized - transaction status, 2 end
+} threadStruct;
+
+
+string getCurrDate() {
+
+	char multiByteString[100];
+	string dateTime;
+
+	time_t t = time(NULL);
+	if (strftime(multiByteString, sizeof(multiByteString), "%T", localtime(&t))) {
+		dateTime = multiByteString;
+	} else {
+		cout << ("Error: Date could not be resolved.") << endl;
+	}
+
+	return dateTime;
+}
+
+void logConsole(bool logging, bool date, string msg, bool std) {
+	if (std) {
+		if (date) {
+			cerr << getCurrDate()+": "+msg << endl;
+		} else {
+			cerr << msg << endl;
+		}
+	} else {
+		if (logging) {
+			if (date) {
+				cout << getCurrDate()+": "+msg;
+			} else {
+				cout << msg;
+			}
+		}
+	}
+}
 
 /*
  * Function prints a error message on cerr and exits program
@@ -160,7 +204,7 @@ bool checkUsersFile(const char *name, string &user, string &pass, bool hashed) {
 	if (users.is_open()) {
 		while (getline(users, line)) {
 			if (returnSubstring(line, "username = ", true) != "") {
-				user = returnSubstring(line, "=", true);
+				user = returnSubstring(line, "username = ", true);
 			} else if (returnSubstring(line, "password = ", true) != "") {
 				pass = returnSubstring(line, "password = ", true);
 			}
@@ -176,13 +220,17 @@ bool checkUsersFile(const char *name, string &user, string &pass, bool hashed) {
 }
 
 
-int getOperation(string message, bool isHashed) {
-	if (message == "NOOP") {
+int getOperation(string message) {
+
+	string op = returnSubstring(message, "\r\n", false);
+
+	if (op == "QUIT") {
 		return 1;
-	} else if (message == "QUIT") {
+	} else if (returnSubstring(op, " ", false) == "USER") {
 		return 2;
+	} else if (returnSubstring(op, " ", false) == "PASS") {
+		return 3;
 	} else {
-		//error op
 		return 0;
 	}
 }
@@ -203,7 +251,7 @@ string generatePidTimeStamp(){
 	
 	time_t currTime = time(NULL);
 	
-	return ("<"+pidStr+"."+to_string(currTime)+"@"+host->h_name);
+	return ("<"+pidStr+"."+to_string(currTime)+"@"+host->h_name+">");
 	
 }
 
@@ -211,7 +259,7 @@ void sendResponse(int socket, bool error, string message) {
 	
 
 	string response;
-	if (error) {
+	if (!error) {
 		response = "+OK";
 	} else {
 		response = "-ERR";
@@ -223,118 +271,182 @@ void sendResponse(int socket, bool error, string message) {
 	send(socket, response.c_str(), response.size(), 0);
 }
 
-void *clientThread(void *threadParam, socket) {
-	
-	int received;
-	auto *tParam = (threadParams *) threadParam;
-	
-	sendResponse(tParam->socket, , 5);
+
+bool checkUser(threadStruct *tS) {
+
+	return (tS->clientUser == serverUser ? true : false);
+}
+
+
+bool authenticateUser(threadStruct *tS) {
+
+	return (tS->clientPass == serverPass ? true : false);
+}
+
+void executeMailServer(bool hash, int op, threadStruct *tS) {
+
+
+	authenticateUser(tS);
+	return;
+}
+
+
+void *clientThread(void *tS) {
+
+	char receivedMessage[1024];
+	auto *tParam = (threadStruct *) tS;
+
+	sendResponse(tParam->commSocket, false, "POP3 server ready "+tParam->pidTimeStamp+"\r\n");
 	for (;;) {
-		if ((received = recv(threadParam->commSocket, receivedMessage, 1024, 0)) <= 0) {
+		if (((int) recv(tParam->commSocket, receivedMessage, 1024, 0)) <= 0) {
 			break;
 		} else {
+
 			int op = 0;
-			op = getOperation(receivedMessage, isHashed);
-			
-			if (isHashed) {
-			
+			if ((op = getOperation(receivedMessage)) != 0) {
+				cout <<  endl << "OP: " << op << endl;
+				if (isHashed) {
+					if (op == 2) {
+						tParam->clientUser = returnSubstring(returnSubstring(receivedMessage, " ", true), "\r\n",
+															 false);
+						if (checkUser(tParam)) {
+							sendResponse(tParam->commSocket, false, "now enter password\r\n");
+
+							if (((int) recv(tParam->commSocket, receivedMessage, 1024, 0)) <= 0) {
+								break;
+							} else {
+								if (getOperation(receivedMessage) == 3) {
+
+									tParam->clientPass = returnSubstring(returnSubstring(receivedMessage, " ", true),
+																		 "\r\n", false);
+									if (authenticateUser(tParam)) {
+										sendResponse(tParam->commSocket, false, "\r\n");
+										tParam->serverStatus = 1;
+
+									} else {
+										sendResponse(tParam->commSocket, true, "invalid username or password\r\n");
+										continue;
+									}
+								} else {
+									sendResponse(tParam->commSocket, true, "wtf jsi retardovany?\r\n");
+								}
+							}
+						} else {
+							sendResponse(tParam->commSocket, true, "invalid username or password\r\n");
+							continue;
+						}
+					} else if (op == 3) {
+						logConsole(true, false, receivedMessage, false);
+						memset(receivedMessage, 0, strlen(receivedMessage));
+					}
+				} else {
+
+				}
 			} else {
-			
+				sendResponse(tParam->commSocket, true, "unknown operation");
+				cout << op << endl;
+				logConsole(true, false, string(receivedMessage), false);
 			}
-			
-			
+
 			// wait for client
 		}
+	}
+	return NULL;
 }
-	
-	int main(int argc, char *argv[]) {
-		
-		
+
+int main(int argc, char *argv[]) {
+
+
 		// params
 		if (!checkParams(argc) || !parseParams(argc, argv)) {
 			throwException("ERROR: Wrong arguments.");
 		}
-		
+
 		if (help) {
 			printHelp();
 			exit(EXIT_SUCCESS);
 		}
-		
-		
+
+
 		cout << "HELP VAL: " << help << endl;
 		cout << "RESET VAL: " << reset << endl;
 		cout << "serverDir VAL: " << serverDir << endl;
 		cout << "usersFile VAL: " << usersFile << endl;
 		cout << "port VAL: " << port << endl;
 		cout << "isHashed VAL: " << isHashed << endl;
-		
+
 		// username and pw from config file
-		if (!checkUsersFile(usersFile.c_str(), user, pass, isHashed)) {
+
+		threadStruct *tS = new threadStruct;
+
+		if (!checkUsersFile(usersFile.c_str(), serverUser, serverPass, isHashed)) {
 			throwException("ERROR: Wrong format of User file.");
 		}
-		
+
+		cout << "USER: " << serverUser << endl;
+		cout << "PASS: " << serverPass << endl;
+
 		cout << generatePidTimeStamp() << endl;
-		
+
 		cout << serverDir << endl;
-		
+
 		// we will create and open a socket
 		int serverSocket;
-		char receivedMessage[1024];
-		
+
 		if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
 			throwException("ERROR: Could not open server socket.\n");
 		}
-		
-		
+
+
 		int opt = 1;
 		if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
 			throwException("ERROR: Setsockopt failure.");
 		}
-		
+
 		struct sockaddr_in clientaddr;
 		struct sockaddr_in serveraddr;
 		memset((char *) &serveraddr, '\0', sizeof(serveraddr));
 		serveraddr.sin_family = AF_INET;
 		serveraddr.sin_port = htons((unsigned short) port);
 		serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-		
+
 		int rc;
 		if ((rc = bind(serverSocket, (struct sockaddr *) &serveraddr, sizeof(serveraddr))) < 0) {
 			throwException("ERROR: Could not bind to port.\n");
 		}
-		
-		
+
+
 		if ((listen(serverSocket, 1)) < 0) {
 			throwException("ERROR: Could not start listening on port.\n");
 		}
 		printf("DEBUG INFO\n");
 		cout << "Port: " << port << endl;;
 		cout << "Server DIR: " << serverDir << endl;
-		
+
 		printf("Server is online! It will be now waiting for request.\n");
-		
+
 		socklen_t clientlen = sizeof(clientaddr);
 		while (1) {
 			int commSocket;
-			int received = 0;
-			
+
 			if ((commSocket = accept(serverSocket, (struct sockaddr *) &clientaddr, &clientlen)) > 0) {
-				
+
 				pthread_t thread;
-				
-				threadParams *thParam = new threadParams;
-					thParam->aPath = usersFile;
-					thParam->cParam = isHashed;
-					thParam->dirPath = serverDir;
-					thParam->socket = commSocket;
-					thParam->tmpPath = "~/tmpPath/";
-					thParam->pidTimeStamp = generatePidTimeStamp();
-				
-				
-				pthread_create(&thread, NULL, clientThread, thParam);
-				
+
+				tS->usersFile = usersFile;
+				tS->isHashed = isHashed;
+				tS->serverDir= serverDir;
+				tS->commSocket = commSocket;
+				tS->tmpDir = "~/tmpPath/";
+				tS->pidTimeStamp = generatePidTimeStamp();
+
+
+				pthread_create(&thread, NULL, clientThread, tS);
+
 			} else {
 				break;
 			}
 		}
-	}
+
+	return 0;
+}
